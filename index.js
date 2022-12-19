be69b0542bcc3499383573d47d430ec32ec3a0f8
Rakib099 <rakibnabi97@gmail.com>
2022-12-19 19:43:40 +0600

    Connected to MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +9,27 @@ const port = process.env.PORT || 5000;
 // middle ware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.i9w8jvi.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('emmysDB').collection('services');
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+// app.get('/services', (req, res) => {
+//     res.send(services);
+// });
 
 app.get('/', (req, res) => {
     res.send("Emmy's Consultancy Server Running");
