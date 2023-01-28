f3ac3821f4c30ec5a477b92df27d4ee9aa8ef609
Rakib099 <rakibnabi97@gmail.com>
2023-01-29 00:48:07 +0600

    JWT installed and verifyJWT function added

--- index.js
+++ index.js
@@ -10,7 +10,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
+function verifyJWT(req, res, next) {
+ const authHeader = req.headers.authentication;
+ if (!authHeader) {
+    return res.status(401).send({message: "Unauthorized Access"});
+ }
+ const token = authHeader.split(' ')[1];
+ jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+    if(err) {
+        return res.status(401).send({message: "Forbidden Access"});
+    }
+    req.decoded = decoded;
+    next();
+ })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.i9w8jvi.mongodb.net/?retryWrites=true&w=majority`;
 
